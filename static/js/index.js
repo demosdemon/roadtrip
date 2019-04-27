17ebaf57b4e17536cf043e9cb5348ebe03a7c7bd
Brandon LeBlanc <brandon@platform.sh>
2019-04-26 20:18:57 -0500

    better location handling

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,87 @@
-let map;
+const root = {
+    lat: 48.868297,
+    lng: 2.353764,
+};
+
+let map, marker;
+let markerDrag = false;
+
+function updateMarker() {
+    if (!markerDrag) {
+        const pos = marker.getPosition();
+        console.log("position_changed");
+        console.log(`lat: ${pos.lat()}, lng: ${pos.lng()}`);
+        // TODO: send position to backend
+    }
+}
+
+function plopMarker(pos) {
+    marker = new google.maps.Marker({
+        animation: google.maps.Animation.DROP,
+        draggable: true,
+        position: pos,
+        map: map,
+    });
+
+    marker.addListener("dragstart", function () {
+        markerDrag = true;
+    });
+
+    marker.addListener("dragend", function () {
+        markerDrag = false;
+        updateMarker();
+    });
+
+    marker.addListener("position_changed", updateMarker);
+
+    updateMarker();
+}
 
 // noinspection JSUnusedGlobalSymbols
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: {
-            lat: 48.868297,
-            lng: 2.353764,
-        },
+        center: root,
         zoom: 6,
+        fullscreenControl: false,
+        gestureHandling: "cooperative",
     });
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            const pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude,
-            };
+    map.addListener("dblclick", function (ev) {
+        marker.setPosition(ev.latLng);
+    });
 
-            map.setCenter(pos);
-        });
+    $("#btn-locate-me").click(function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(geolocationCallback, geolocationErrorCallback);
+        } else {
+            handleLocationError(false, map.getCenter());
+        }
+    });
+
+    plopMarker(root);
+}
+
+function handleLocationError(browserHasGeolocation, pos) {
+    const infoWindow = new google.maps.InfoWindow();
+    infoWindow.setPosition(pos);
+    if (browserHasGeolocation) {
+        infoWindow.setContent("Error: the geolocation service failed.");
+    } else {
+        infoWindow.setContent("Error: your browser doesn't support geolocation.")
     }
+    infoWindow.open(map);
+}
+
+function geolocationCallback(position) {
+    const pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+    };
+    map.panTo(pos);
+    marker.setPosition(pos);
+}
+
+function geolocationErrorCallback(e) {
+    console.dir(e);
+    handleLocationError(true, map.getCenter());
 }
